df91b5e1983cf01ffb4ad0a85089f50ad1e99eb5
Vladimir Zaytsev <vladimir.zaytsev.m@gmail.com>
2023-10-23 11:34:27 -0500

    Clear color in krypta example

--- examples/krypta.rs
+++ examples/krypta.rs
@@ -26,7 +26,7 @@ pub struct Movable;
 fn main() {
     // Basic setup.
     App::new()
-        .insert_resource(ClearColor(Color::rgb_u8(0, 0, 0)))
+        .insert_resource(ClearColor(Color::rgba_u8(0, 0, 0, 0)))
         .add_plugins((
             DefaultPlugins
                 .set(AssetPlugin {
