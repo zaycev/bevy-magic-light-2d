899106a8250672999348a43c19a7c28e2eec2ae1
Vladimir Zaytsev <vladimir.zaytsev.m@gmail.com>
2023-11-04 21:24:32 -0500

    Migrate to Bevy12

--- src/gi/util.rs
+++ src/gi/util.rs
@@ -5,22 +5,28 @@ use crate::gi::WORKGROUP_SIZE;
 
 pub struct AssetUtil;
 
-impl AssetUtil {
-    pub fn gi(l: &'static str) -> AssetPath {
+impl AssetUtil
+{
+    pub fn gi(l: &'static str) -> AssetPath
+    {
         AssetPath::parse("gi/target".into()).with_label(l.to_owned())
     }
-    pub fn camera(l: &'static str) -> AssetPath {
+    pub fn camera(l: &'static str) -> AssetPath
+    {
         AssetPath::parse("camera/target".into()).with_label(l.to_owned())
     }
-    pub fn material(l: &'static str) -> AssetPath {
+    pub fn material(l: &'static str) -> AssetPath
+    {
         AssetPath::parse("material".into()).with_label(l.to_owned())
     }
-    pub fn mesh(l: &'static str) -> AssetPath {
+    pub fn mesh(l: &'static str) -> AssetPath
+    {
         AssetPath::parse("mesh".into()).with_label(l.to_owned())
     }
 }
 
-pub fn align_to_work_group_grid(size: IVec2) -> IVec2 {
+pub fn align_to_work_group_grid(size: IVec2) -> IVec2
+{
     let wg_size = WORKGROUP_SIZE as i32;
     size + IVec2::new(wg_size - size.x % wg_size, wg_size - size.y % wg_size)
 }
